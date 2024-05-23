5c263a17793b1464801c15f4593149c711609fea
LeoGidev <leogimenez.dev@gmail.com>
2024-05-23 17:56:20 -0300

    se trabaja en funciones

--- detector.py
+++ detector.py
@@ -7,9 +7,6 @@ mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-# Inicializa los puntos de referencia faciales fuera de la función principal.
-#face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
-
 def select_image():
     try:
         # Seleccionar un archivo de imagen.
@@ -32,9 +29,41 @@ def process_image(image_path):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Procesar la imagen y extraer los puntos de referencia faciales.
-        #results = face_mesh.process(image_rgb)
+        face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
+        results = face_mesh.process(image_rgb)
 
-        # Resto del código para procesar y mostrar la imagen...
+        # Dibujar los puntos de referencia faciales en la imagen.
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                # Dibujar la malla facial completa.
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
+
+                # Dibujar contornos específicos de ojos, nariz y boca.
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
+
+                # Dibujar puntos de referencia específicos (ojos, labios e iris).
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
+
+        # Mostrar la imagen con los puntos de referencia.
+        cv2.imshow('Image with facial landmarks', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     except Exception as e:
         messagebox.showerror("Error", f"Error al procesar la imagen: {str(e)}")
 
@@ -55,6 +84,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 

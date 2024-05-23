4eea339b59f8f104ef88061ea8959777adcd850d
LeoGidev <leogimenez.dev@gmail.com>
2024-05-23 17:20:06 -0300

    se inicia mediapipe

--- detector.py
+++ detector.py
@@ -2,3 +2,7 @@ import cv2
 import mediapipe as mp
 from tkinter import Tk, Button, Label, filedialog
 from PIL import Image, ImageTk
+
+# Inicializar MediaPipe
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic

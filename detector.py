7526d17e44443e50003cca8fd52242d234fde3cc
LeoGidev <leogimenez.dev@gmail.com>
2024-05-23 18:00:46 -0300

    se trabaja en funciones

--- detector.py
+++ detector.py
@@ -15,14 +15,30 @@ def select_image():
             filetypes=(("Archivos JPG", "*.jpg"), ("Todos los archivos", "*.*"))
         )
 
-        
+        if not image_path:
+            messagebox.showinfo("Información", "No se seleccionó ninguna imagen.")
+        else:
+            process_image(image_path)
     except Exception as e:
         messagebox.showerror("Error", f"Error al seleccionar la imagen: {str(e)}")
 
+def process_image(image_path):
+    try:
+        # Cargar la imagen
+        image = cv2.imread(image_path)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+        # Procesar la imagen y extraer los puntos de referencia faciales.
+        face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
+        results = face_mesh.process(image_rgb)
+
+        # Resto del código para procesar y mostrar la imagen...
+    except Exception as e:
+        messagebox.showerror("Error", f"Error al procesar la imagen: {str(e)}")
 
 def main():
     try:
+        print("Antes del bucle principal")
         # Crear la ventana principal.
         root = Tk()
         root.withdraw()  # Ocultar la ventana principal.
@@ -33,6 +49,7 @@ def main():
 
         # Mostrar la ventana principal.
         root.mainloop()
+        print("Después del bucle principal")
     except Exception as e:
         messagebox.showerror("Error", f"Error al iniciar la aplicación: {str(e)}")
 

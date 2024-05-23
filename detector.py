e18243ee4a1a5ea00660a29aae5e9a9474cc1953
LeoGidev <leogimenez.dev@gmail.com>
2024-05-23 17:57:22 -0300

    se trabaja en funciones

--- detector.py
+++ detector.py
@@ -15,57 +15,11 @@ def select_image():
             filetypes=(("Archivos JPG", "*.jpg"), ("Todos los archivos", "*.*"))
         )
 
-        if not image_path:
-            messagebox.showinfo("Información", "No se seleccionó ninguna imagen.")
-        else:
-            process_image(image_path)
+        
     except Exception as e:
         messagebox.showerror("Error", f"Error al seleccionar la imagen: {str(e)}")
 
-def process_image(image_path):
-    try:
-        # Cargar la imagen
-        image = cv2.imread(image_path)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Procesar la imagen y extraer los puntos de referencia faciales.
-        face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
-        results = face_mesh.process(image_rgb)
-
-        # Dibujar los puntos de referencia faciales en la imagen.
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                # Dibujar la malla facial completa.
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
-
-                # Dibujar contornos específicos de ojos, nariz y boca.
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
-
-                # Dibujar puntos de referencia específicos (ojos, labios e iris).
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_IRISES,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
-
-        # Mostrar la imagen con los puntos de referencia.
-        cv2.imshow('Image with facial landmarks', image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    except Exception as e:
-        messagebox.showerror("Error", f"Error al procesar la imagen: {str(e)}")
 
 def main():
     try:

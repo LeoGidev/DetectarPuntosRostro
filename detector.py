f257e72a75a9395007330e170dae2fcb3553f6ad
LeoGidev <leogimenez.dev@gmail.com>
2024-05-23 18:23:35 -0300

    se trabaja en funciones

--- detector.py
+++ detector.py
@@ -1,11 +1,10 @@
 import cv2
 import mediapipe as mp
 from tkinter import Tk, filedialog, messagebox, Button
+import os
 
 # Inicializa MediaPipe Face Mesh.
 mp_face_mesh = mp.solutions.face_mesh
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 
 def select_image():
     try:
@@ -24,12 +23,26 @@ def select_image():
 
 def process_image(image_path):
     try:
-        # Cargar la imagen
+        # Verificar si la ruta del archivo es válida.
+        if not os.path.exists(image_path):
+            messagebox.showerror("Error", "La ruta del archivo de imagen no es válida.")
+            return
+        
+        # Cargar la imagen.
         image = cv2.imread(image_path)
+        
+        # Verificar si la imagen se cargó correctamente.
+        if image is None:
+            messagebox.showerror("Error", "No se pudo cargar la imagen.")
+            return
+
+        # Convertir la imagen a RGB (MediaPipe requiere imágenes en RGB).
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Procesar la imagen y extraer los puntos de referencia faciales.
+        # Inicializar MediaPipe Face Mesh.
         face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5)
+
+        # Procesar la imagen y extraer los puntos de referencia faciales.
         results = face_mesh.process(image_rgb)
 
         # Resto del código para procesar y mostrar la imagen...
@@ -41,7 +54,6 @@ def main():
         print("Antes del bucle principal")
         # Crear la ventana principal.
         root = Tk()
-        #root.withdraw()  # Ocultar la ventana principal.
 
         # Crear el botón para seleccionar una imagen.
         select_image_button = Button(root, text="Seleccionar imagen", command=select_image)
@@ -55,4 +67,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
